84bd607e9a494c87144e23cffd57337aa0e9284f
Dumitru Vlad-Mihai <vlad_liviu22@gmail.com>
2025-04-26 12:29:04 +0300

    clean_text function to bring content to an easy-to-work-with form

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import pandas as pd
 def get_info(file):
     file.info()
 
+def clean_text(value):
+    if pd.isna(value):
+        return ""
+    
+    value = str(value).lower()
+    value = value.replace("  ", " ")
+    return value
+
+
 opened_file = pd.read_parquet("veridion_product_deduplication_challenge.snappy.parquet")
+opened_file.to_csv("veridion_product_deduplication_challenge.snappy.csv")
 
 
 get_info(opened_file)
